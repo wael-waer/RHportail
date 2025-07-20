a3208b4a130cac4d2037abab990a50d2360e085e
wael-waer <122459813+wael-waer@users.noreply.github.com>
2025-07-20 15:30:59 +0100

    update

--- src/API/PortailRH.API/Models/DataBase/Candidature.cs
+++ src/API/PortailRH.API/Models/DataBase/Candidature.cs
@@ -1,15 +1,13 @@
 namespace PortailRH.API.Models.DataBase
 {
-
-
     public class Candidature : EntityBase
     {
-
         public string Nom { get; set; } = default!;
         public string Prenom { get; set; } = default!;
         public string Email { get; set; } = default!;
+        public string PosteSouhaite { get; set; } = default!;
 
-        public string PosteSouhaite { get; set; }
+        public string CVUrl { get; set; } = default!;
         
 
     }
