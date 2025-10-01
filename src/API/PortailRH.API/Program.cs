582b05f6c1933845689c0d53c12caffc4a800940
wael-waer <122459813+wael-waer@users.noreply.github.com>
2025-10-01 11:14:40 +0100

    commit last

--- src/API/PortailRH.API/Program.cs
+++ src/API/PortailRH.API/Program.cs
@@ -174,7 +174,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
